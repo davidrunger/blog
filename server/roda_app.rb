5e73586155a55e25a336bf8ecd56cdc6163ddf8c
David Runger <davidjrunger@gmail.com>
2024-07-07 15:30:17 -0500

    Autocorrect RuboCop violations

--- server/roda_app.rb
+++ server/roda_app.rb
@@ -12,8 +12,5 @@ class RodaApp < Roda
   # Some Roda configuration is handled in the `config/initializers.rb` file.
   # But you can also add additional Roda configuration here if needed.
 
-  route do |r|
-    # Load Roda routes in server/routes (and src/_routes via `bridgetown-routes`)
-    r.bridgetown
-  end
+  route(&:bridgetown)
 end
