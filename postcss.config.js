fea65a32005522f2913c6477dc4a789dd7ac8843
David Runger <davidjrunger@gmail.com>
2025-01-24 05:12:04 -0600

    Remove almost all postcss plugins
    They don't seem to be having any obvious positive effect, and I think
    that at least one of them was having a negative effect.

--- postcss.config.js
+++ postcss.config.js
@@ -2,21 +2,5 @@ module.exports = {
   parser: 'postcss-scss',
   plugins: {
     '@tailwindcss/postcss': {},
-    'postcss-easy-import': {},
-    'postcss-mixins': {},
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 2,
-      features: {
-        'nesting-rules': true,
-        'custom-media-queries': true
-      },
-    },
-    'cssnano' : {
-      preset: 'default'
-    },
   }
 }
