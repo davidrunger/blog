b719123ea6e4b009297373ea4147bd1e7786dd8c
David Runger <davidjrunger@gmail.com>
2025-01-24 05:39:45 -0600

    Bump tailwindcss from 3.4.17 to 4.0.0 (#475)
    It's kind of outrageous that I am paying this much page weight for
    Tailwind, and this much complexity, when I think I'm not even using it
    at all. Still, I want to have it available...

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,6 @@
 module.exports = {
   parser: 'postcss-scss',
   plugins: {
-    'tailwindcss': {},
-    'postcss-easy-import': {},
-    'postcss-mixins': {},
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 2,
-      features: {
-        'nesting-rules': true,
-        'custom-media-queries': true
-      },
-    },
-    'cssnano' : {
-      preset: 'default'
-    },
+    '@tailwindcss/postcss': {},
   }
 }
