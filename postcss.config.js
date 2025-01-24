7766322d1ad5d5b35386c87a59f58592f497e6a9
David Runger <davidjrunger@gmail.com>
2025-01-24 04:39:44 -0600

    Update postcss.config.js with @tailwindcss/postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: 'postcss-scss',
   plugins: {
-    'tailwindcss': {},
+    '@tailwindcss/postcss': {},
     'postcss-easy-import': {},
     'postcss-mixins': {},
     'postcss-flexbugs-fixes': {},
